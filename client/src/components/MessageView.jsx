e0d171de2be9da95dc9f204227c26720d10e06e7
ryananger <anger.ryan.s@gmail.com>
2024-09-17 21:07:38 -0400

    input style

--- client/src/components/MessageView.jsx
+++ client/src/components/MessageView.jsx
@@ -7,10 +7,17 @@ import Message from './Message.jsx';
 const MessageView = function() {
   const messages = st.user.messages[st.messageWith];
 
+  var handleSubmit = function() {
+
+  };
+
   return (
     <div className='messageView v c'>
       <Message message={messages[0]}/>
-      <input id='messageInput'/>
+      <div id='messageInputContainer' className='h'>
+        <textarea id='messageInput' className='h'/>
+        <div className='v c'>send</div>
+      </div>
     </div>
   );
 };
