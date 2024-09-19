2df46dc0a6f2283982ee9c1238aabd59e156110e
ryananger <anger.ryan.s@gmail.com>
2024-09-19 16:34:07 -0400

    fixes

--- client/src/icons/icons.js
+++ client/src/icons/icons.js
@@ -2,12 +2,16 @@ import {AiOutlineLogout as LogoutIcon,
         AiOutlineLogin as LoginIcon,
         AiFillSetting as SettingsIcon,
         AiOutlineClose as CloseIcon} from 'react-icons/ai';
+import {BsPersonFillAdd as AddFriendIcon,
+        BsPersonFillExclamation as PendFriendIcon} from 'react-icons/bs';
 
 const icons = {
   LoginIcon: LoginIcon,
   LogoutIcon: LogoutIcon,
   SettingsIcon: SettingsIcon,
-  CloseIcon: CloseIcon
+  CloseIcon: CloseIcon,
+  AddFriendIcon: AddFriendIcon,
+  PendFriendIcon: PendFriendIcon
 };
 
 export default icons;
