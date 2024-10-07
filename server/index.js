e5f2bea55702833e7bbcf7fa21894523fcb310b8
ryananger <anger.ryan.s@gmail.com>
2024-10-07 19:42:50 -0400

    secure socket

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 
-//const https  = require('https');
+const https  = require('https');
 const http   = require('http');
 const fs     = require('fs');
 const cors   = require('cors');
@@ -10,9 +10,6 @@ const path   = require('path');
 const router = express.Router();
 const app    = express();
 const {initSocket} = require('./io.js');
-const server = http.createServer(app);
-
-initSocket(server);
 
 const controller = require('./controller.js');
 
@@ -28,10 +25,10 @@ router.get('/:url', function(req, res) {
   res.sendFile(dist + '/index.html');
 });
 
-// var options = {
-//   key:  fs.readFileSync(process.env.HTTPS_KEY),
-//   cert: fs.readFileSync(process.env.HTTPS_CERT)
-// };
+var options = {
+  key:  fs.readFileSync(process.env.HTTPS_KEY),
+  cert: fs.readFileSync(process.env.HTTPS_CERT)
+};
 
 const validateClient = function (req, res, next) {
   const authCheck = req.headers.auth;
@@ -62,9 +59,13 @@ app.post('/api/users/acceptFriend', controller.acceptFriend);
 app.post('/api/messages/', controller.sendMessage);
 app.post('/api/messages/clearConversation', controller.clearConversation);
 
-
 const PORT = 4001;
 
-server.listen(PORT);
-//https.createServer(options, app).listen(443);
+const server = https.createServer(options, app);
+
+http.createServer(app).listen(PORT);
+server.listen(443);
+
+initSocket(server);
+
 console.log(`Server listening at http://localhost:${PORT}`);
